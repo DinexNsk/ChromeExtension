4cbdcda3caf70ae439416fef8763e80bb9c526eb
DinexNsk <dinex.nsk@gmail.com>
2018-04-02 00:26:21 +0700

    added file for routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { EventPageComponent } from './event-page/event-page.component';
+import { PopupComponent } from './popup/popup.component';
+
+const routes: Routes = [
+  { path: 'event-page', component: EventPageComponent },
+  { path: 'popup', component: PopupComponent },
+  { path: '', redirectTo: 'popup', pathMatch: 'full' }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { useHash:true})],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
