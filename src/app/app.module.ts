509e5967c1bd90ca096d99c9048853a58cb78147
DinexNsk <dinex.nsk@gmail.com>
2018-04-02 00:28:05 +0700

    fix for app.modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http'
 
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { ExtensionService } from './extension.service';
+import { EventPageComponent } from './event-page/event-page.component';
+import { PopupComponent } from './popup/popup.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EventPageComponent,
+    PopupComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [ExtensionService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
